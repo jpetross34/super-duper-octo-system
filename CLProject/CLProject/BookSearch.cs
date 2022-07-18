b32c9c2b8a8746e794d05ee60f18a7ec51186057
James Petross <jamespetross@gmail.com>
2022-07-18 16:51:32 -0400

    starting to put create the console

--- CLProject/CLProject/BookSearch.cs
+++ CLProject/CLProject/BookSearch.cs
@@ -1,35 +1,40 @@
-﻿using Google.Apis.Books.v1;
-using Google.Apis.Books.v1.Data;
-using Google.Apis.Services;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Books.v1;
+using Google.Apis.Books.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
 
 namespace CLProject
 {
     public static class BookSearch
     {
-        public static BooksService service = new BooksService(new BaseClientService.Initializer)
+        private static string API_KEY = "AIzaSyBqpPwjvFuErq4UOaA1Z2o0fIkKkCi0miU";
+
+        public static BooksService service = new BooksService(new BaseClientService.Initializer
         {
-            ApplicationName = "ISBNBookSearch";
-            ApiKey = "AIzaSyBqpPwjvFuErq4UOaA1Z2o0fIkKkCi0miU";
-        }
+            ApplicationName = "ISBNBookSearch",
+            ApiKey = API_KEY,
+        });
 
         public static async Task<Volume> SearchISBN(string isbn)
         {
-            Console.WriteLine("Executing a book search request...");
+            Console.WriteLine("Executing a book search request for ISBN: {0} ...", isbn);
 
             var result = await service.Volumes.List(isbn).ExecuteAsync();
 
             if (result != null && result.Items != null)
             {
                 var item = result.Items.FirstOrDefault();
-
                 return item;
             }
-
             return null;
         }
     }
--- CLProject/CLProject/Program.cs
+++ CLProject/CLProject/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CLProject
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.WriteLine("\n------------------------------------------------------------------------");
+
+            string isbn = "0071807993";
+            var output = BookSearch.SearchISBN(isbn);
+            var result = output.Result;
+
+            Console.WriteLine("\n------------------------------------------------------------------------");
+            Console.WriteLine("Book Name: \t" + result.VolumeInfo.Title);
+            Console.WriteLine("Author: \t" + result.VolumeInfo.Authors.FirstOrDefault());
+            Console.WriteLine("Publisher: \t" + result.VolumeInfo.Publisher);
         }
     }
 }
